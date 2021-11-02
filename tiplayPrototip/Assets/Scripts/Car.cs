2490648b70a9519741f7df8dea8b69ef23cf54e3
Calquter <73954471+Calquter@users.noreply.github.com>
2021-11-02 15:11:26 +0300

    First Push

---
+++ tiplayPrototip/Assets/Scripts/Car.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Car
+{
+
+    public float carHealth;
+    protected float carSpeed;
+
+
+    public abstract void CarCrash();
+    public abstract void AvoidObstacles();
+
+}
